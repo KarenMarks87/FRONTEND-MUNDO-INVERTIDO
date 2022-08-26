5b6e812031bcba0a3cfdf087ca09840e0f9518ad
KarenMarks87 <101564311+KarenMarks87@users.noreply.github.com>
2022-08-26 10:34:17 -0300

    Add files via upload

---
+++ SEMANA-FRONTEND-MUNDO-INVERTIDO/assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBXGevGojU_JI95LBYXlD2DXvwg7czUWdE",
+    authDomain: "projeto-teste-cdf8b.firebaseapp.com",
+    projectId: "projeto-teste-cdf8b",
+    storageBucket: "projeto-teste-cdf8b.appspot.com",
+    messagingSenderId: "718979699666",
+    appId: "1:718979699666:web:a181694a74034ef255ee0b"
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig)
+  export default app
+  
